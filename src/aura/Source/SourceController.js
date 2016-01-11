2a37bd46d57f655effd36481cc6bc39923ff0a68
Jason Bury <jbury@salesforce.com>
2016-01-11 14:44:22 -0600

    Adding report column dropdown to campaign list ui
    In order to know which Contact/Lead record to use from a report row, we
    must know which column to look at to find the Contact/Lead id.
    
    This adds a dropdown to the UI that will let the user specify the column
    when they are defining a report source segment.
    
    Modifying Source.cmp markup to specify CampaignListBuilder_CTRL as a
    controller.
    
    Modifying Source.cmp markup to render a ui:inputSelect as an inline form
    element whenever rendering the autocomplete input for a report source
    segment.
    
    Adding an event handler to Source.cmp markup to handle the change event
    whenever the component's segment gets updated.
    
    Adding handleSourceChange() method to SourceController.js that gets
    invoked whenever the Source component's segment gets changed.  This
    method checks to see if the current segment is a report source segment,
    and if so it calls the getReportIdColumns() method on the component's
    controller (CampaignListBuilder_CTRL).  If the getReportIdColumns()
    method returns a collection of columns, it uses that collection to build
    the inputSelect options for the column name component.  If there was an
    error for any reason, including the source report doesn't exist, it
    populates the column name component with a non-option.  This behavior
    should probably be changed as a future enhancement... i.e. handle
    different types of errors differently.
    
    Adding handleColumnName() method to SourceController.js to update the
    component's segment with the columnName chosen in the column name
    inputSelect.  If no column is chosen, we remove the columnName property
    from the segment.
    
    Adding getReportIdColumns() AuraEnabled method to
    CampaignListBuilter_CTRL.  This method is called by the lightning
    component with a report id and delegates to the ReportService to return
    id columns in that report.  If the ReportService fails for any reason
    (throws an exception), then an exception is thrown in this method which
    is checked for in the lightning component as an error status.
    
    Adding getIdColumns() method to ReportService.  This method will
    retrieve the metadata for the given report id, and pluck out any columns
    that have type 'ID_DATA'.  These are added to a collection and returned.
    Note that the columns returned by
    ReportExtendedMetadata.getDetailColumnInfo() are actually the columns
    available as part of the report type, not necessarily the columns
    currently visible in the defined report via the Salesforce UI.
    
    Adding tests for new methods.
    
    Adding ReportService interface.

--- src/aura/Source/SourceController.js
+++ src/aura/Source/SourceController.js
@@ -1,8 +1,62 @@
 ({
+    handleSourceChange: function(component, event, helper) {
+        var sourceSegment = event.getParam("value");
+        if ('REPORT_SOURCE_SEGMENT' !== sourceSegment.segmentType) return;
+        var columnNameComponent = component.find("columnName");
+        var action = component.get("c.getReportIdColumns");
+        action.setParams({"reportId": sourceSegment.sourceId});
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var columns = response.getReturnValue();
+                var options = [{
+                    label: 'Select Id Column',
+                    value: ''
+                }];
+                values = Object.keys(columns);
+                for (i = 0; i < values.length; i++) {
+                    var columnLabel = columns[values[i]];
+                    var columnId = values[i];
+                    var isSelected = (sourceSegment.columnName === values[i]);
+                    options.push({
+                        label: columnLabel,
+                        value: columnId,
+                        selected: isSelected
+                    });
+                }
+                component.find("columnName").set("v.options", options);
+            }
+            else if (state === "ERROR") {
+                // @todo what do we do if we can't find any id columns or the
+                // report doesn't exist, etc?
+                component.find("columnName").set(
+                    "v.options",
+                    [{
+                        label: '--',
+                        value: ''
+                    }]
+                );
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
     handleSourceType: function(component, event, helper) {
         var selectCmp = component.find("sourceType");
         var source = component.get('v.source');
         source.segmentType = selectCmp.get("v.value");
+        component.set("v.source", source);
+    },
+
+    handleColumnName: function(component, event, helper) {
+        var selectCmp = component.find("columnName");
+        var source = component.get('v.source');
+        var columnName = selectCmp.get("v.value");
+        if ('' === columnName) {
+            delete source.columnName;
+        } else {
+            source.columnName = columnName;
+        }
         component.set("v.source", source);
     },
 
@@ -17,11 +71,11 @@
 
     },
 
-    handleAutocomplete: function(component, event) {
-        var source = component.get('v.source');
+    handleAutocomplete: function(component, event, helper) {
         var selOpt = event.getParam('selectedOption');
-        source.sourceName = selOpt.label;
-        source.sourceId = selOpt.value;
-        component.set("v.source", source);
-    },
+        var sourceSegment = component.get("v.source");
+        sourceSegment.sourceName = selOpt.label;
+        sourceSegment.sourceId = selOpt.value;
+        component.set("v.source", sourceSegment);
+    }
 })
