19858b77c7cf5a7dd57d34d45be80a5868711939
Jason Bury <jbury@salesforce.com>
2016-01-15 11:10:03 -0600

    Fix bug where report columns would not render on first load
    The report columns dropdown options were not being populated when
    loading a report source where that report source was not the root
    segment.  This was because the valueChange event does not get called
    when loading these children (for some reason, i dont know why?).  I have
    moved the column dropdown populating code to a helper method, and now
    delegate to this helper method from the valueChange handler in the
    controller.  Additionally, I also make sure to call this helper method
    from the renderer, after rendering is first completed.

--- src/aura/Source/SourceController.js
+++ src/aura/Source/SourceController.js
@@ -1,44 +1,7 @@
 ({
     handleSourceChange: function(component, event, helper) {
         var sourceSegment = event.getParam("value");
-        if ('REPORT_SOURCE_SEGMENT' !== sourceSegment.segmentType) return;
-        var columnNameComponent = component.find("columnName");
-        var action = component.get("c.getReportIdColumns");
-        action.setParams({"reportId": sourceSegment.sourceId});
-        action.setCallback(this, function(response) {
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                var columns = response.getReturnValue();
-                var options = [{
-                    label: 'Select Id Column',
-                    value: ''
-                }];
-                values = Object.keys(columns);
-                for (i = 0; i < values.length; i++) {
-                    var columnLabel = columns[values[i]];
-                    var columnId = values[i];
-                    var isSelected = (sourceSegment.columnName === values[i]);
-                    options.push({
-                        label: columnLabel,
-                        value: columnId,
-                        selected: isSelected
-                    });
-                }
-                component.find("columnName").set("v.options", options);
-            }
-            else if (state === "ERROR") {
-                // @todo what do we do if we can't find any id columns or the
-                // report doesn't exist, etc?
-                component.find("columnName").set(
-                    "v.options",
-                    [{
-                        label: '--',
-                        value: ''
-                    }]
-                );
-            }
-        });
-        $A.enqueueAction(action);
+        helper.updateReportColumns(component, helper, sourceSegment);
     },
 
     handleSourceType: function(component, event, helper) {
